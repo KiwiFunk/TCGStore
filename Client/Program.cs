60411e911692cdacff5cb09f654777c7e992163a
Alex <alex_p97@live.com>
2025-06-12 16:56:36 +0100

    Updated Client program.cs to use HttpClient

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
+using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Blazor uses HttpClient to interact with the APIs
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5271/") // Localhost URL for the server
+});
 
 await builder.Build().RunAsync();
